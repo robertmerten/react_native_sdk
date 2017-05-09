bfe2c78176ca109ba60982017743025fc052bf27
Obaied <abdullah.obaied@gmail.com>
2017-05-09 10:32:33 +0200

    Change CommandListener implementation to support a callback to JS side

--- example_test/android/app/src/main/java/com/adjust/examples/CommandListener.java
+++ example_test/android/app/src/main/java/com/adjust/examples/CommandListener.java
@@ -3,30 +3,35 @@ package com.adjust.examples;
 import android.content.Context;
 import android.util.Log;
 
-import com.adjust.testlibrary.ICommandListener;
+import com.facebook.react.bridge.*;
+import com.facebook.react.modules.core.*;
+
+import com.adjust.testlibrary.ICommandJsonListener;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-public class CommandListener implements ICommandListener {
-    AdjustCommandExecutor adjustCommandExecutor;
+public class CommandListener implements ICommandJsonListener {
+    final Callback mCallback;
 
-    public CommandListener(Context context) {
-        adjustCommandExecutor = new AdjustCommandExecutor(context);
+    public CommandListener(Context context, final Callback callback) {
+        mCallback = callback;
     }
 
     @Override
-    public void executeCommand(String className, String methodName, Map<String, List<String>> parameters) {
-        switch (className) {
-            case "Adjust":
-                adjustCommandExecutor.executeCommand(new Command(className, methodName, parameters));
-                break;
-            default:
-                debug("Could not find %s class to execute", className);
-                break;
-        }
+    public void executeCommand(String className, String methodName, String jsonParameters) {
+        mCallback.invoke(className, methodName, jsonParameters);
+
+        //switch (className) {
+            //case "Adjust":
+                //adjustCommandExecutor.executeCommand(new Command(className, methodName, parameters));
+                //break;
+            //default:
+                //debug("Could not find %s class to execute", className);
+                //break;
+        //}
     }
 
     static void debug(String message, Object... parameters) {
