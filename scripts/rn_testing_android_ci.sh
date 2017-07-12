d792eb45f1716e83aa2b3cc89cafcdd1c2e6afae
Obaied <abdullah.obaied@gmail.com>
2017-07-12 14:14:47 +0200

    Modify test scripts

--- scripts/rn_testing_android_ci.sh
+++ scripts/rn_testing_android_ci.sh
@@ -4,9 +4,9 @@
 #set -e
 
 # Get the current directory (/scripts/ directory)
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Traverse up to get to the root directory
-SDK_DIR="$(dirname "$SCRIPTS_DIR")"
+SDK_DIR="$(dirname "$SDK_DIR")"
 EXAMPLE_DIR=example_ci
 SDK_NAME=react-native-adjust
 
@@ -53,9 +53,19 @@ rsync -a . ${EXAMPLE_DIR}/node_modules/${SDK_NAME} --exclude=example --exclude=e
 
 # Establish link
 echo -e "${GREEN}>>> Establish linkage to ${SDK_NAME} ${NC}"
-cd ${EXAMPLE_DIR}
+cd ${SDK_DIR}/${EXAMPLE_DIR}
 react-native link ${SDK_NAME}
 
-echo -e "${GREEN}>>> Building & Running on Android ${NC}"
-cd ${SDK_DIR}/${EXAMPLE_DIR}
 react-native run-android
+
+#echo -e "${GREEN}>>> Building & Running on Android ${NC}"
+#cd ${SDK_DIR}/${EXAMPLE_DIR}
+#react-native bundle --dev false --platform android --entry-file index.android.js --bundle-output ./android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest ./android/app/build/intermediates/res/merged/debug
+#(cd android; ./gradlew assembleDebug)
+
+## copy apk to scripts dir
+#cd ${SDK_DIR}/${EXAMPLE_DIR}
+#cp -v android/app/build/outputs/apk/app-debug.apk ${SDK_DIR}/scripts/app.apk
+
+#cd ${SDK_DIR}/${EXAMPLE_DIR}
+#adb install -r android/app/build/outputs/apk/app-debug.apk
